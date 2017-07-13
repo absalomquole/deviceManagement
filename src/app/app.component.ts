cb43759708a8441f694ddbdac22a596e58bf9b28
Pramanick, Shubham (US - Bengaluru) <shupramanick@deloitte.com>
2017-07-13 10:07:04 +0530

    package.json update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Device Lab';
-}
+  name:String;
+  items:any;
+  isTrue:Boolean;
+  constructor(){
+    console.log("const called");
+    this.isTrue=false;
+    this.name="default";
+    this.items=[
+      { name:"shubha",class:"we"},
+      { name:"shubha32",class:"we234"},
+      { name:"shubha64",class:"we756"},
+      { name:"shubha73",class:"we534"},
+      { name:"s234hubha",class:"we23"}];
+   
+  }
+    ngOnInit(){
+      console.log("const2 called");
+    }
+    ngOnChanges(){
+      console.log("const3 called");
+    }
+    newClickReg(a:String):void{
+        this.name=a;
+    }
+    newClickwe(her:any):void{
+      her.style.color="red";
+    }
+
+ }
