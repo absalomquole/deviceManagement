e4b726921b2191a7f7fc52315c640d5f902b6497
Shubham Pramanick <insanesanky@gmail.com>
2017-07-30 02:28:38 +0530

    service and access origin

--- src/app/getdevicedata.service.ts
+++ src/app/getdevicedata.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class GetdevicedataService {
@@ -8,15 +11,20 @@ export class GetdevicedataService {
   constructor(private http:Http) { 
 
   }
-  getDeviceData(){
-    let deviceData;
-    console.log("deviceDataCalled");
-    this.http.request('/api/deviceData').subscribe((res: Response) => {
-      deviceData=res.json();
+  getDeviceData(): Observable<any[]>{
+
+    return this.http.get('https://devicemanagement.herokuapp.com/api/deviceData')
+    .map((res:Response) => res.json())
+    .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+
+    // let deviceData;
+    // console.log("deviceDataCalled");
+    // this.http.request('/api/deviceData').subscribe((res: Response) => {
+    //   deviceData=res.json();
       
-    });
-    return deviceData
-    // return this.http.get(`/api/deviceData`).map((res:Response) => {res.json();console.log(res.json())});
+    // });
+    // return deviceData
+    // // return this.http.get(`/api/deviceData`).map((res:Response) => {res.json();console.log(res.json())});
   }
 
 }
