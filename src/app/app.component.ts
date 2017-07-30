871f44ac9fc4dbd82d2dfb163210aa262c0c5b68
Shubham Pramanick <insanesanky@gmail.com>
2017-07-30 13:22:30 +0530

    access origin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Subscription }   from 'rxjs/Subscription';
 import {MailserviceService} from './mailservice.service';
 import {GetdevicedataService} from './getdevicedata.service'
 
@@ -10,6 +11,7 @@ import {GetdevicedataService} from './getdevicedata.service'
   providers: [MailserviceService, GetdevicedataService]
 })
 export class AppComponent {
+  private subscriptions: Subscription[] = [];
   name:String;
   items:any;
   isTrue:Boolean;
@@ -17,6 +19,16 @@ export class AppComponent {
   deviceData:any;
   veryberry:String;
   constructor(private _MailserviceService: MailserviceService,private _GetdevicedataService: GetdevicedataService) {
+
+
+    this.subscriptions.push(this._GetdevicedataService.getDeviceData()
+    .subscribe((data) => {
+        console.log(data);
+    }, (err) => {
+      console.log(err);
+    }
+));
+
     this.deviceData=_GetdevicedataService.getDeviceData();
     console.log("this.deviceData");
     console.log(this.deviceData);
@@ -31,6 +43,8 @@ export class AppComponent {
       { name:"shubha64",class:"we756"},
       { name:"shubha73",class: "we534"},
       { name:"s234hubha",class: "we23"}];
+
+      
   }
     ngOnInit(){
       console.log("const2 called");
